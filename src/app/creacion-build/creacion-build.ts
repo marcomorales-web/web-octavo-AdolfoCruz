be365acf0ef7945956777018b6cb58bd4fc80ef8
adolfocruz-jpg <adolfo.cruz@itsvc.edu.mx>
2026-02-26 11:13:43 -0600

    Actualizacion-Nueva

--- src/app/creacion-build/creacion-build.ts
+++ src/app/creacion-build/creacion-build.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-creacion-build',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './creacion-build.html',
-  styleUrl: './creacion-build.css',
+  styleUrls: ['./creacion-build.css'],
+  animations: [
+    trigger('fadeSlide', [
+      transition(':enter', [
+        style({
+          opacity: 0,
+          transform: 'translateY(30px)'
+        }),
+        animate('600ms ease-out', style({
+          opacity: 1,
+          transform: 'translateY(0)'
+        }))
+      ])
+    ])
+  ]
 })
 export class CreacionBuild {
+  currentStep = 1;
 
+  nextStep() {
+    if (this.currentStep < 4) {
+      this.currentStep++;
+    }
+  }
+
+  prevStep() {
+    if (this.currentStep > 1) {
+      this.currentStep--;
+    }
+  }
 }
